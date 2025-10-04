b59b993cda66a29bc2677b74733ce223ac436852
canalesmejiad <david.kanale@hotmail.es>
2025-10-04 12:37:20 -0700

    cm

--- scripts/product.js
+++ scripts/product.js
@@ -7,12 +7,11 @@ const products = [
 ];
 
 const productSelect = document.getElementById("product");
-
-products.forEach(product => {
-    const option = document.createElement("option");
-    option.value = product.id;
-    option.textContent = product.name;
-    productSelect.appendChild(option);
-});
-
-document.getElementById("year").textContent = new Date().getFullYear();
+if (productSelect) {
+    products.forEach(prod => {
+        const option = document.createElement("option");
+        option.value = prod.id;
+        option.textContent = prod.name;
+        productSelect.appendChild(option);
+    });
+}
